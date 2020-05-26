bb524295b81cdec0562855dd60602abb45cc00d0
KC Cai <kc.c@lifebyte.com.cn>
2020-05-26 11:31:41 +0800

    update 菜单

--- src/store/store.js
+++ src/store/store.js
@@ -7,41 +7,61 @@ const state = {
             type: 'md-home', // icon类型
             text: '主页', // 文本内容
         },
-        // {
-        //     text: '二级菜单',
-        //     type: 'ios-paper',
-        //     name: 'other',
-        //     children: [
-        //         {
-        //             type: 'ios-grid',
-        //             name: 't1',
-        //             text: '表格',
-        //             // hidden 属性 隐藏此菜单 可以通过在地址栏上输入对应的 URL 来显示页面
-        //             // hidden: true,
-        //         },
-        //         {
-        //             text: '三级菜单',
-        //             type: 'ios-paper',
-        //             children: [
-        //                 {
-        //                     type: 'ios-notifications-outline',
-        //                     name: 'msg',
-        //                     text: '查看消息',
-        //                 },
-        //                 {
-        //                     type: 'md-lock',
-        //                     name: 'password',
-        //                     text: '修改密码',
-        //                 },
-        //                 {
-        //                     type: 'md-person',
-        //                     name: 'userinfo',
-        //                     text: '基本资料',
-        //                 },
-        //             ],
-        //         },
-        //     ],
-        // },
+        {
+            text: '会场管理',
+            type: 'md-bowtie',
+            size: 18, // icon大小
+            category: 'all',
+            children: [
+                {
+                    type: 'md-wine',
+                    size: 18, // icon大小
+                    text: '会场设置',
+                    children: [
+                        {
+                            type: 'md-wine',
+                            size: 18, // icon大小
+                            name: '',
+                            text: '模板选择',
+                        },
+                        {
+                            type: 'md-wine',
+                            size: 18, // icon大小
+                            name: 'seat-setting',
+                            text: '模板定义',
+                        }
+                    ]
+                },
+                {
+                    type: 'ios-people',
+                    size: 18, // icon大小
+                    name: '',
+                    text: '名单模板',
+                    children: [
+                        {
+                            type: 'ios-people',
+                            size: 18, // icon大小
+                            name: '',
+                            text: '模板下载',
+                        },
+                        {
+                            type: 'ios-people',
+                            size: 18, // icon大小
+                            name: '',
+                            text: '服务选择'
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            text: '其他功能',
+            type: 'md-folder',
+            size: 18, // icon大小
+            category: 'all',
+            name: ''
+            // 互动留言*互动抽奖*宴会反馈*菜品评价*菜品推荐*电子请帖*互动游戏*
+        },
         {
             text: '账号管理',
             type: 'md-body',
@@ -51,45 +71,63 @@ const state = {
                 {
                     type: 'md-person',
                     size: 18, // icon大小
-                    name: 'info',
-                    text: '基本资料',
-                }
-            ]
-        },
-        {
-            text: '功能管理',
-            type: 'md-folder',
-            size: 18, // icon大小
-            category: 'god',
-            children: [
-                {
-                    type: 'md-global',
-                    size: 18, // icon大小
-                    name: 'function-setting',
-                    text: '功能设置',
-                }
-            ]
-        },
-        {
-            text: '会场管理',
-            type: 'md-bowtie',
-            size: 18, // icon大小
-            category: 'all',
-            children: [
-                {
-                    type: 'ios-people',
-                    size: 18, // icon大小
-                    name: 'upload-data',
-                    text: '会场选择',
+                    name: '',
+                    text: '忘记密码',
                 },
                 {
-                    type: 'md-wine',
+                    type: 'md-person',
                     size: 18, // icon大小
-                    name: 'seat-setting',
-                    text: '会场设置',
+                    name: '',
+                    text: '更改密码',
+                },
+                {
+                    type: 'md-person',
+                    size: 18, // icon大小
+                    name: '',
+                    text: '增值服务',
                 }
             ]
-        }
+        },
+        {
+            text: '账号服务',
+            type: 'md-bookmarks',
+            size: 18, // icon大小
+            category: 'admin',
+            children: [
+                {
+                    type: 'md-bookmarks',
+                    size: 18, // icon大小
+                    name: 'info',
+                    text: '基本资料',
+                },
+                {
+                    type: 'md-bookmarks',
+                    size: 18, // icon大小
+                    name: 'info',
+                    text: '主账号管理',
+                },
+                {
+                    type: 'md-bookmarks',
+                    size: 18, // icon大小
+                    name: 'info',
+                    text: '子账号管理',
+                },
+                {
+                    type: 'md-bookmarks',
+                    category: 'god',
+                    size: 18, // icon大小
+                    name: 'info',
+                    text: '操作日志',
+                },
+                {
+                    type: 'md-bookmarks',
+                    category: 'god',
+                    size: 18, // icon大小
+                    name: 'info',
+                    text: '系统监控',
+                }
+            ]
+        },
     ],
     adminInfo: {
         admin_token: '',
