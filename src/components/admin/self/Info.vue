57a3da65419da487718c1d9232cfcfc252c68a55
ChockCong <363384526@qq.com>
2020-06-11 23:15:45 +0800

    feat 主账号

--- src/components/admin/self/Info.vue
+++ src/components/admin/self/Info.vue
@@ -1,115 +1,186 @@
 <template>
     <div class="index-vue-info">
         <div class="img-line">
-            <div class="user-img-c">
+            <!-- <div class="user-img-c">
                 <Icon type="logo-android" />
+            </div> -->
+            <div class="button-wrap">
+                <Button type="warning" @click="functionModel = true">会员升级</Button>
+                <Button type="info"  @click="functionModel = true">会员续费</Button>
             </div>
-            <div class="helper"></div>
         </div>
         <div class="content-wrap">
-            <div class="button-wrap">
-                <Button type="warning">会员升级</Button>
-                <Button type="info">会员续费</Button>
+            <Table v-if="!edit" width="502" :show-header="false" :disabled-hover="true" :columns="columns" :data="datas">
+                <template slot-scope="{ row, index }" slot="content">
+                    <div class="content-box">
+                        <Tag color="blue">{{index === 1 ? level(row.content) : row.content}}</Tag>
+                    </div>
+                </template>
+            </Table>
+            <Table :key="'tableEdit'" v-else  width="502" :show-header="false" :disabled-hover="true" :columns="columns" :data="datas">
+                <template slot-scope="{ row, index }" slot="content">
+                    <div class="content-box">
+                        <Tag v-if="index === 1" color="blue">{{level(row.content)}}</Tag>
+                        <Tag v-if="index === 1" color="primary">修改</Tag>
+                        <Input v-if="index !== 1" type="text" v-model="row.content" />
+                    </div>
+                </template>
+            </Table>
+            <div class="button-box">
+                <Button type="primary" @click="editInfo">{{ edit ? '保存信息' : '编辑信息' }}</Button>
             </div>
-            <ul>
-                <li>
-                    <span>用户名</span>
-                    <Tag color="green">{{adminInfo.username}}</Tag>
-                </li>
-                <li>
-                    <span>电子邮箱</span>
-                    <Tag color="green">{{adminInfo.email}}</Tag>
-                </li>
-                <li>
-                    <span>电话号码</span>
-                    <Tag color="green">{{adminInfo.phoneNo}}</Tag>
-                </li>
-                <li>
-                    <span>公司名字</span>
-                    <Tag color="green">{{adminInfo.company.name}}</Tag>
-                </li>
-                <li>
-                    <span>公司地址</span>
-                    <Tag color="green">{{adminInfo.company.address}}</Tag>
-                </li>
-                <li>
-                    <span>权限</span>
-                    <Tag color="green">{{level(adminInfo.company.level)}}</Tag>
-                </li>
-            </ul>
         </div>
+        <Modal
+            v-model="functionModel"
+            :title="'功能分配'"
+            :loading="true"
+            :width="700"
+            @on-ok="() => { this.functionModel = true }"
+            @on-cancel="() => { this.functionModel = false }">
+            <FunctionList :sales="true" @confirm="confirmFunction"></FunctionList>
+        </Modal>
     </div>
 </template>
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters } from 'vuex';
+import FunctionList from '../../common/FunctionList';
 export default {
     name: 'Info',
+    components: {FunctionList},
+    data() {
+        return {
+            edit: false,
+            functionModel: false,
+            columns: [{
+                    title: '类目',
+                    key: 'title',
+                    width: 100,
+                    align: 'center'
+                },
+                {
+                    title: '内容',
+                    key: 'content',
+                    align: 'left',
+                    slot: 'content',
+                    width: 400
+                }],
+            datas: []
+        }
+    },
     computed: {
         ...mapGetters({
 			adminInfo: 'getAdminInfo'
         })
+    },
+    methods: {
+        editInfo() {
+            this.edit = !this.edit;
+            this.$forceUpdate();
+        },
+        confirmFunction(selection) {
+            let select = selection;
+            console.log(select);
+        }
+    },
+    beforeMount() {
+        this.datas = [{
+            title: '用户名',
+            content: this.adminInfo.username,
+            cellClassName: {
+                title: 'title-cell',
+                content: 'info-cell',
+            }
+        },{
+            title: '会员权限',
+            content: this.adminInfo.level,
+            cellClassName: {
+                title: 'title-cell',
+                content: 'info-cell',
+            }
+        },{
+            title: '邮箱地址',
+            content: this.adminInfo.email,
+            cellClassName: {
+                title: 'title-cell',
+                content: 'info-cell',
+            }
+        },{
+            title: '电话号码',
+            content: this.adminInfo.phoneNo,
+            cellClassName: {
+                title: 'title-cell',
+                content: 'info-cell',
+            }
+        },{
+            title: '公司名称',
+            content: this.adminInfo.company.name,
+            cellClassName: {
+                title: 'title-cell',
+                content: 'info-cell',
+            }
+        },{
+            title: '公司地址',
+            content: this.adminInfo.company.address,
+            cellClassName: {
+                title: 'title-cell',
+                content: 'info-cell',
+            }
+        }];
     }
 }
 </script>
 <style lang="scss" scoped>
+.appear-enter-active, .appear-leave-active {
+    transition: all .5s ease;
+}
+.appear-enter, .appear-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    transform: translateY(-10px);
+    opacity: 0;
+}
 .index-vue-info {
     height: 100%;
+    padding: 20px;
     & .img-line {
         position: relative;
         display: flex;
         align-items: center;
-        height: 100px;
-        padding-top: 50px;
         & .user-img-c {
-            position: absolute;
-            width: 100px;
-            height: 100px;
+            width: 50px;
+            height: 50px;
             background: gray;
             border-radius: 50%;
             overflow: hidden;
             display: flex;
             justify-content: center;
+            margin-right: 10px;
             align-items: center;
-            left: 0;
-            right: 0;
-            margin: 0 auto;
             & i {
-                font-size: 50px;
+                font-size: 30px;
                 color: #6af64a
             }
         }
-        & .helper {
-            width: 100%;
-            height: 1px;
-            border: 1px solid gray;
+        & button {
+            margin-right: 10px;
         }
+        margin-bottom: 20px;
     }
     & .content-wrap {
-        padding-top: 60px;
-        position: relative;
-        display: flex;
-        justify-content: center;
-        & .button-wrap {
-            position: absolute;
-            top: 10px;
-            right: 20px;
-            & button {
-                margin-left: 10px;
-            }
-        }
-        & ul {
+        & .button-box {
+            padding: 20px 0;
             text-align: left;
-            & li {
-                font-size: 16px;
-                margin-bottom: 10px;
-                list-style-type:none;
-                & span{
-                    width: 100px;
-                    display: inline-block;
-                }
-                & .ivu-tag {
-                    font-size: 14px;
-                }
+        }
+        ::v-deep .title-cell {
+            // background-image: linear-gradient(to bottom right, #2d8cf0, #9ddafb);
+            background-image: linear-gradient(to bottom right, #20222A, #515a6e);
+            color: white;
+            font-weight: bold;
+        }
+        ::v-deep .info-cell {
+            overflow: hidden;
+            display: flex;
+            align-items: center;
+            & .ivu-table-cell {
+                width: 100%;
             }
         }
     }
