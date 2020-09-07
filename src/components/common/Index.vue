c15f40713aca2fde7d85c825a269d830cf1e2d00
KC Cai <kc.c@lifebyte.com.cn>
2020-09-07 16:33:28 +0800

    add 首页page

---
+++ src/components/common/Index.vue
@@ -0,0 +1,172 @@
+<template>
+<div class="index-vue-main">
+    <div class="ad-carousel">
+        <Carousel autoplay v-model="carousel" :autoplay-speed="2000" loop>
+            <CarouselItem>
+                <div class="demo-carousel">
+                    <img src="https://www1.djicdn.com/cms/uploads/2e2375062b1f0a7a717358036c6b381c.jpg" />
+                </div>
+            </CarouselItem>
+            <CarouselItem>
+                <div class="demo-carousel">
+                    <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3492285453,982599627&fm=26&gp=0.jpg" />
+                </div>
+            </CarouselItem>
+        </Carousel>
+    </div>
+    <div class="index-vue-main-content">
+        <div class="content-title">码雷将为你提供更便捷的服务</div>
+        <div class="content-block">
+            <template v-for="(item, index) in showItems">
+                <div class="content-item" :key="index">
+                    <!-- <transition name="layout">
+                        <div v-if="item.hoverIt" class="lay-out"></div>
+                    </transition> -->
+                    <img :src="item.img" @mouseenter="hoverImg(index, true)"  @mouseout="hoverImg(index, false)" />
+                    <div class="text-box theme-light">
+                        <div class="headline">{{item.name}}</div>
+                        <div class="desc">{{item.desc}}</div>
+                        <div href="" class="link-box">
+                            <div class="text">了解更多</div>
+                            <div class="icon fa fa-angle-right">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Index',
+    data() {
+        return {
+            showItems: [
+                { name: '会场系统', desc: '宴会/会议便捷解决方案', img: 'https://www1.djicdn.com/cms/uploads/bccdb5d218eb2c32dccb5c88babc7679@1x.webp', hoverIt: false },
+                { name: '视频互动', desc: '婚宴会场的互动新模式', img: 'https://www1.djicdn.com/cms/uploads/bccdb5d218eb2c32dccb5c88babc7679@1x.webp', hoverIt: false },
+                { name: '专业影像', desc: '天地一体的专业拍摄解决方案', img: 'https://www1.djicdn.com/cms/uploads/bccdb5d218eb2c32dccb5c88babc7679@1x.webp', hoverIt: false },
+                { name: '会场系统', desc: '天地一体的专业拍摄解决方案', img: 'https://www1.djicdn.com/cms/uploads/bccdb5d218eb2c32dccb5c88babc7679@1x.webp', hoverIt: false },
+                { name: '会场系统', desc: '天地一体的专业拍摄解决方案', img: 'https://www1.djicdn.com/cms/uploads/bccdb5d218eb2c32dccb5c88babc7679@1x.webp', hoverIt: false }],
+            hoverIt: false
+        }
+    },
+    methods: {
+        hoverImg(index, type) {
+            this.showItems[index].hoverIt = type;
+            this.$forceUpdate();
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.layout-enter-active {
+  transition: all .2s ease;
+}
+.layout-leave-active {
+  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.layout-enter, .layout-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+.index-vue-main {
+    width: 100%;
+    &-content {
+        width: 1200px;
+        box-sizing: border-box;
+        margin: 0 auto;
+        padding: 10px 0;
+        /* height: 100%; */
+         & .content-title {
+            font-size: 40px;
+            line-height: 44px;
+            font-weight: 500;
+            color: rgba(0,0,0,.85);
+            text-align: center;
+            padding: 20px 0;
+        }
+        & .content-block {
+            display: flex;
+            flex-wrap: wrap;
+            & .content-item {
+                width: 389px;
+                height: 512px;
+                display: block;
+                margin: 0 5px 10px;
+                overflow: hidden;
+                position: relative;
+                color: white;
+                &:nth-child(3n) {
+                    margin-right: 0
+                }
+                & .lay-out {
+                    position: absolute;
+                    height: 100%;
+                    width: 100%;
+                    background: rgba(0,0,0,.4);
+                    z-index: 1;
+                }
+                & img {
+                    max-width: 100%;
+                    transition: all .6s ease;
+                    &:hover {
+                        transform: scale(1.2);
+                    }
+                }
+                & .text-box {
+                    padding: 0 48px;
+                    position: absolute;
+                    z-index: 2;
+                    top: 48px;
+                    width: 100%;
+                    & .headline {
+                        font-size: 32px;
+                    }
+                    & .desc {
+                        font-size: 16px;
+                    }
+                    &:hover {
+                        // font-weight: bold;
+                        background: rgba(0,0,0,.8);
+                        // height: 100%;
+                    }
+                }
+                & .link-box {
+                    position: relative;
+                    z-index: 400;
+                    text-align: center;
+                    margin-top: 16px;
+                    display: inline-flex;
+                    align-items: center;
+                    transition: all .3s ease;
+                    cursor: pointer;
+                    &:hover {
+                        text-decoration: underline;
+                    }
+                    margin-left: 8px;
+                    font-size: 16px;
+                }
+            }
+        }
+    }
+    & .ad-carousel {
+        & .demo-carousel {
+            height: 500px;
+            line-height: 500px;
+            text-align: center;
+            color: #fff;
+            font-size: 20px;
+            // background: lightgray;
+            & img {
+                width: 100%;
+                height: 100%
+            }
+        }
+    }
+}
+</style>
