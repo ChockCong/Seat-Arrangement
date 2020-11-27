d3bde0651a901024e2ac0b2ad5dbed867a1c41af
KC Cai <kc.c@lifebyte.com.cn>
2020-11-27 18:22:11 +0800

    整理账号信息

--- src/utils/oss.js
+++ src/utils/oss.js
@@ -2,8 +2,8 @@ var OSS = require('ali-oss')
 export function client() {
     var client = new OSS({
         region: 'oss-cn-shenzhen',
-        accessKeyId: 'LTAI4GGLtKktjfXuqT2sq5Lx',
-        accessKeySecret: 'x9osjP3ugZTF4RPIz4Etj0TtREGn1B',
+        accessKeyId: process.env.VUE_APP_ALI_OSS_ID,
+        accessKeySecret: process.env.VUE_APP_ALI_OSS_SECRET,
         bucket: 'testcodethunder'
     }) //后端提供数据
     return client
